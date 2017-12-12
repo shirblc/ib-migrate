b3a213247e5ea032bda87e2b20f9e5254f202f0c
Eliram Haklai <eliramh@gmail.com>
2017-12-12 14:43:14 +0200

    Start Parsing Posts and comments

--- convert.py
+++ convert.py
@@ -4,9 +4,85 @@ This script converts Israblog html (complete) backup files to an XML file
 import logging
 import re
 import os
+import codecs
 
 BACKUP_FOLDER = '/users/eliram/Documents/israblog'
 TEMPLATE_FILE = 'template.html'
+
+STATE_OUT = 1
+STATE_POST = 2
+STATE_COMMENT = 3
+
+
+class BlogPost(object):
+    def __init__(self, post_id):
+        """
+
+        :param str post_id:
+        """
+        self.post_id = post_id  # type: str
+        self.title = None  # type: str
+        self.body = None  # type: str
+        self.ts = None  # type: float
+        self.comments = {}
+
+
+class BlogComment(object):
+    def __init__(self, comment_id):
+        """
+
+        :param str comment_id:
+        """
+        self.comment_id = comment_id
+        self.name = None
+        self.ts = None
+
+
+class ParseBackupFile(object):
+    """
+    Parse the HTML
+
+    """
+
+    def __init__(self, backup_file_text):
+        """
+
+        :param list backup_file_text: a list of lines
+        """
+        self.backup_file_text = backup_file_text  # type: str
+        self.state = STATE_OUT
+        self.previous_state = self.state
+        self.current_blog_post = None
+        self.current_blog_comment = None
+
+    def clear_data(self):
+        self.current_blog_post = None
+        self.current_blog_comment = None
+
+    def process_row(self, row):
+        pass
+
+    def process(self):
+        rows = self.backup_file_text.splitlines()
+        for row in rows:
+            self.process_row(row)
+            self.previous_state = self.state
+
+
+
+def parse_backup_files(backup_files_list):
+    """
+    :param list backup_files_list: a list of html file names
+    :return: all parsed data
+    :rtype: dict
+    """
+    parsed_data = {}
+    for filename in backup_files_list:
+        with codecs.open(os.path.join(BACKUP_FOLDER, filename), encoding='cp1255') as backup_file:
+            file_text = backup_file.readlines()
+        parse_obj = ParseBackupFile(file_text)
+        parsed_data.update(parse_obj.process())
+    return parsed_data
 
 
 def get_backup_files():
@@ -28,18 +104,6 @@ def get_backup_files():
     logging.info('Found %d backup files to process', len(file_list))
     logging.debug(file_list)
     return file_list
-
-
-def parse_backup_files(backup_files_list):
-    """
-
-    :param list backup_files_list: a list of html file names
-    :type backup_files_list:
-    :return: all parsed data
-    :rtype: dict
-    """
-    parsed_data = {}
-    return parsed_data
 
 
 def save_parsed_data(parsed_data):
